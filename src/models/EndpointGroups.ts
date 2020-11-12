23ebd1326e1598b181116245f3c5f9f293861d86
Dade Cook <dadecook@gmail.com>
2020-11-11 19:12:04 -0500

    Clean slate to start over!

--- src/models/EndpointGroups.ts
+++
@@ -1,5 +0,0 @@
-import { ApiMetadata } from './ApiMetadata';
-
-export interface EndpointGroups {
-	[groupName: string]: ApiMetadata[];
-}
--- src/models/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './ApiMetadata';
-export * from './EndpointGroups';
-export * from './Parameter';
-export * from './Schema';
